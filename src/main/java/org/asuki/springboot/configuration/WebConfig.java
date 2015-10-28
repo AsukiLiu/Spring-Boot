4b655ef3c3695fd961a330f31ce8ebaed057883a
Andy Liu <andyhome.liu@gmail.com>
2015-10-29 00:27:29 +0900

    Add ServletFilter

--- src/main/java/org/asuki/springboot/configuration/WebConfig.java
+++ src/main/java/org/asuki/springboot/configuration/WebConfig.java
@@ -1,12 +1,22 @@
 package org.asuki.springboot.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @Configuration
@@ -26,5 +36,30 @@ public class WebConfig extends WebMvcConfigurationSupport {
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(customJackson2HttpMessageConverter());
         super.addDefaultHttpMessageConverters(converters);
+    }
+
+    @Bean
+    public FilterRegistrationBean countFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+
+        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        filterRegistrationBean.setFilter(new GenericFilterBean() {
+
+            private final Logger log = LoggerFactory.getLogger(GenericFilterBean.class);
+
+            private long counter;
+
+            @Override
+            public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+                    throws IOException, ServletException {
+
+                counter++;
+                log.info("count:{}", counter);
+
+                chain.doFilter(req, res);
+            }
+        });
+
+        return filterRegistrationBean;
     }
 }
